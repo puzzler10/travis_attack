7ba79f953541b50d9530f25721c961c8c48b1573
Tom Roth <tom.roth@live.com.au>
2021-05-07 15:09:22 +1000

    results_df now working

--- utils.py
+++ utils.py
@@ -1,3 +1,7 @@
+import GPUtil
+from threading import Thread
+import time 
+
 def show_random_elements(dataset, num_examples=10):
     """Print some elements in a nice format so you can take a look at them. Use for a dataset from the `datasets` package.  """
     import datasets
@@ -18,3 +22,88 @@ def show_random_elements(dataset, num_examples=10):
         if isinstance(typ, datasets.ClassLabel):
             df[column] = df[column].transform(lambda i: typ.names[i])
     display(HTML(df.to_html()))
+
+    
+def round_t(t, dp=2):
+    """Return rounded tensors for easy viewing. t is a tensor, dp=decimal places"""
+    if t.device.type == "cuda": t=t.cpu()
+    return t.detach().numpy().round(dp)                       
+    
+    
+    
+    
+    
+################### GPU functions ######################    
+def print_device_info(): 
+    """
+    Prints some statistics around versions and the GPU's available for
+    the host machine
+    """
+    import torch
+    import sys
+    print("######## Diagnostics and version information ######## ")
+    print('__Python VERSION:', sys.version)
+    print('__pyTorch VERSION:', torch.__version__)
+    print('__CUDA VERSION', )
+    from subprocess import call
+    # call(["nvcc", "--version"]) does not work
+    #! nvcc --version
+    print('__CUDNN VERSION:', torch.backends.cudnn.version())
+    print('__Number CUDA Devices:', torch.cuda.device_count())
+    print('__Devices')
+    call(["nvidia-smi", "--format=csv", "--query-gpu=index,name,driver_version,memory.total,memory.used,memory.free"])
+    print('Active CUDA Device: GPU', torch.cuda.current_device())
+    print ('Available devices ', torch.cuda.device_count())
+    print("Device name:", torch.cuda.get_device_name())
+    print ('Current cuda device ', torch.cuda.current_device())
+    print("#################################################################")
+    
+    
+def dump_tensors(gpu_only=True):
+    """Prints a list of the Tensors being tracked by the garbage collector. 
+    Useful when running into an out of memory error on the GPU. """
+    import gc
+    total_size = 0
+    for obj in gc.get_objects():
+        try:
+            if torch.is_tensor(obj):
+                if not gpu_only or obj.is_cuda:
+                    print("%s:%s%s %s" % (type(obj).__name__, 
+                                            " GPU" if obj.is_cuda else "",
+                                            " pinned" if obj.is_pinned else "",
+                                            pretty_size(obj.size())))
+                    total_size += obj.numel()
+            elif hasattr(obj, "data") and torch.is_tensor(obj.data):
+                if not gpu_only or obj.is_cuda:
+                    print("%s → %s:%s%s%s%s %s" % (type(obj).__name__, 
+                                                    type(obj.data).__name__, 
+                                                    " GPU" if obj.is_cuda else "",
+                                                    " pinned" if obj.data.is_pinned else "",
+                                                    " grad" if obj.requires_grad else "", 
+                                                    " volatile" if obj.volatile else "",
+                                                    pretty_size(obj.data.size())))
+                    total_size += obj.data.numel()
+        except Exception as e:
+            pass        
+    print("Total size:", total_size)   
+    
+    
+       
+class Monitor(Thread):
+    """Use this to check that you are using the GPU during your pytorch functions and to track memory usage 
+    of the GPU's as well.""" 
+    def __init__(self, delay):
+        super(Monitor, self).__init__()
+        self.stopped = False
+        self.delay = delay # Time between calls to GPUtil
+        self.start()
+
+    def run(self):
+        while not self.stopped:
+            GPUtil.showUtilization()
+            time.sleep(self.delay)
+
+    def stop(self):
+        self.stopped = True
+        
+
