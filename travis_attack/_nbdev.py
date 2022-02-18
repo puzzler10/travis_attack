cb20689808b651b03ec1e215abbbf1685fe19804
Tom Roth <tom.roth@live.com.au>
2022-02-18 16:11:49 +1100

    fleshed out config, model and data files a bit more

--- travis_attack/_nbdev.py
+++ travis_attack/_nbdev.py
@@ -8,13 +8,20 @@ index = {"timecode": "00_utils.ipynb",
          "Monitor": "00_utils.ipynb",
          "show_gpu": "00_utils.ipynb",
          "round_t": "00_utils.ipynb",
-         "show_random_elements": "10_data_cleaning.ipynb",
+         "Config": "03_config.ipynb",
+         "prepare_models": "07_models.ipynb",
+         "RottenTomatoesDataset": "10_data.ipynb",
+         "SimpleDatasetWrapper": "10_data.ipynb",
+         "get_vm_probs": "20_trainer.ipynb",
          "plot_grad_flow": "30_logging.ipynb",
          "print_info_on_generated_text": "30_logging.ipynb",
          "check_parameters_update": "30_logging.ipynb"}
 
 modules = ["utils.py",
+           "config.py",
+           "models.py",
            "data.py",
+           "trainer.py",
            "logging.py"]
 
 doc_url = "https://puzzler10.github.io/travis_attack/"
